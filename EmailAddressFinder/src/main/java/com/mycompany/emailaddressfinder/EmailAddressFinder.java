f2436aa08353daa4ece019a5b2752f284052df71
Andrew <andrew_mnayerji@hotmail.com>
2015-06-04 19:20:58 -0400

    Changed to accept the command line argument...added minor error handling...formatted output...changed domain name to a data member...added protocol

--- EmailAddressFinder/src/main/java/com/mycompany/emailaddressfinder/EmailAddressFinder.java
+++ EmailAddressFinder/src/main/java/com/mycompany/emailaddressfinder/EmailAddressFinder.java
@@ -12,42 +12,108 @@ import org.jsoup.select.Elements;
 
 public class EmailAddressFinder {
 
-    private Set<String> emailAddresses = new HashSet();
+    private final Set<String> emailAddresses = new HashSet();
+    private final String domainName;
+    private final String protocol;
 
     public static void main(String[] args) throws Exception {
-        EmailAddressFinder finder = new EmailAddressFinder();
-        finder.findEmailsOnDomain("http://www.iana.org");
+        EmailAddressFinder finder = new EmailAddressFinder(args[0]);
+        boolean success = finder.findEmailsOnDomain();
+        if (!success) {
+            System.exit(1);
+        }
+    }
+
+    EmailAddressFinder(String domain) {
+        String[] parts = domain.split("//");
+        //separate the protocol from the domain
+        if (parts.length > 1) {
+            protocol = parts[0];
+            domainName = parts[1].replace("www.", "");
+        } else {
+            domainName = domain.replace("www.", "");
+            //defaults to http
+            protocol = "http:";
+        }
     }
 
     //ties it all together
-    public void findEmailsOnDomain(String domainName) throws IOException {
+    //does not work if the protocol or the domain are invalid
+    //does not look through links in pages more than one level deep because that might cause an endless loop or might cause going through the entire internet - kept it simple
+    public boolean findEmailsOnDomain() {
         //assuming the protocol and domain name are valid
-        Document doc = Jsoup.connect(domainName).data("query", "Java").userAgent("Mozilla").cookie("auth", "token").timeout(3000).get();
-        getEmailAddresses(doc.toString());
-        Elements links = getPageLinks(doc);
-        for(Element link : links)
-        {
-            String linkAddress = link.attr("abs:href");
-            if(!linkAddress.contains("protocols"))
-                getEmailAddresses(Jsoup.connect(linkAddress).data("query", "Java").userAgent("Mozilla").cookie("auth", "token").timeout(3000).get().toString());
+        boolean flag = true;
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(getFullAddress()).data("query", "Java").userAgent("Mozilla").cookie("auth", "token").timeout(3000).get();
+        } catch (Exception e) {
+            System.out.println("An error occurred while getting e-mails from page '" + getFullAddress() + "':" + e.getMessage());
+            //e.printStackTrace();
+            flag = false;
         }
-        for(String str : emailAddresses)
-            System.out.println(str);
+        if(doc != null)
+        {
+            getEmailAddresses(doc.toString());
+            Elements links = getPageLinks(doc);
+            for (Element link : links) {
+                String linkAddress = link.attr("abs:href");
+                try {
+                    getEmailAddresses(Jsoup.connect(linkAddress).data("query", "Java").userAgent("Mozilla").cookie("auth", "token").timeout(3000).get().toString());
+                } catch (IOException e) {
+                    System.out.println("An error occurred while getting e-mails from page '" + linkAddress + "':" + e.getMessage());
+                    //e.printStackTrace();
+                    flag = false;
+                }
+            }
+            showOutput();
+        }
+        return flag;
     }
 
     //returns all links on a page
-    public Elements getPageLinks(Document doc) throws IOException {
-        Elements resultLinks = doc.select("a"); 
-       return resultLinks;
+    public Elements getPageLinks(Document doc) {
+        Elements resultLinks = doc.select("a");
+        return resultLinks;
     }
 
+    //finds email addreses from an html string and adds them to a set
     public void getEmailAddresses(String html) {
         Matcher m = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+").matcher(html);
         while (m.find()) {
             String newAddress = m.group();
-            if (!emailAddresses.contains(newAddress)) {
-                emailAddresses.add(newAddress);
+            try {
+                //make sure I am getting distinct e-mail addresses
+                if (!emailAddresses.contains(newAddress)) {
+                    //not sure if we need all addresses that need that use that domain name - if we do, I need to uncomment the next 4 commented lines
+                    //if (newAddress.length() > domainName.length()) {
+                    //    if (newAddress.substring(newAddress.length() - domainName.length()).equals(domainName)) {
+                    emailAddresses.add(newAddress);
+                    //    }
+                    //}
+                }
+            } catch (Exception e) {
+                System.out.println("An error occured while parsing e-mail address '" + newAddress + "': " + e.getMessage());
+                //e.printStackTrace();
             }
         }
+    }
+    
+    //simply gets the full link to the original page provided by the user
+    private String getFullAddress() {
+        return protocol + "//www." + domainName;
+    }
+    
+    //shows the user the result of the search
+    private void showOutput()
+    {
+        if(emailAddresses.size() > 0)
+        {
+            System.out.println("LIST OF EMAIL ADDRESSES ON '" + getFullAddress() + "':");
+            for (String str : emailAddresses) {
+                System.out.println(str);
+            }
+        }
+        else
+            System.out.println("WE COULDN'T FIND ANY EMAIL ADDRESSES ON '" + getFullAddress() + "':");
     }
 }
